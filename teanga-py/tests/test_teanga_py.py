e09543463a89dc4cd9e1ac51f8111a5918f8c6a3
John P. McCrae <john@mccr.ae>
2024-04-05 16:48:15 +0100

    Improve test

--- teanga-py/tests/test_teanga_py.py
+++ teanga-py/tests/test_teanga_py.py
@@ -1,7 +1,8 @@
-import teanga_pyo3.teanga # if this fails the Rust code is not installed
+import teanga_pyo3.teanga as teangadb# if this fails the Rust code is not installed
 import teanga
 
 def test_create_corpus():
+    teangadb.Corpus("tmp.db")
     corpus = teanga.Corpus(db="tmp.db", new=True)
     corpus.add_layer_meta("text")
     print(corpus.meta["text"].base)
